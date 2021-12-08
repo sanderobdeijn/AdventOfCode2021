1ff567490ea04922b3ad46374b25c25d7bf863b4
Sander Obdeijn <sander@xprtz.net>
2021-12-08 15:44:10 +0100

    day03

---
+++ AdventOfCode2021.Tests/Day03/ChallengeTests.cs
@@ -0,0 +1,60 @@
+﻿namespace AdventOfCode2021.Tests.Day03;
+
+using AdventOfCode2021.Day03;
+
+public class ChallengeTests
+{
+    [Fact]
+    public void TestMostCommonBit()
+    {
+        Assert.Equal("1", Challenge.GetMostCommonBit(new List<char> { '1' }));
+        Assert.Equal("0", Challenge.GetMostCommonBit(new List<char> { '0' }));
+        Assert.Equal("1", Challenge.GetMostCommonBit(new List<char> { '1', '0', '1' }));
+    }
+
+
+    [Fact]
+     public void TestExample()
+    {
+        var challenge = new Challenge(@"D:\Development\AdventOfCode\AdventOfCode2021\AdventOfCode2021.Tests\Day03\example.txt");
+
+        Assert.NotEmpty(challenge.DiagnosticReports);
+        Assert.Equal(12, challenge.DiagnosticReports.Count);
+
+        var bitsGroupedByPosition = Challenge.GetBitsGroupedByPosition(challenge.DiagnosticReports);
+
+        Assert.Equal("011110011100", bitsGroupedByPosition.First());
+        Assert.Equal(5, bitsGroupedByPosition.Count());
+
+        Assert.Equal("10110", Challenge.GetMostCommonBits(bitsGroupedByPosition));
+
+        Assert.Equal(22, challenge.GetGammaRate());
+        Assert.Equal(9, challenge.GetEpsilonRate());
+        Assert.Equal(198, challenge.GetPowerConsumption());
+        Assert.Equal(23, challenge.GetOxygenRating());
+        Assert.Equal(10, challenge.GetCO2ScrubberRating());
+        Assert.Equal(230, challenge.GetLifeSupportRating());
+    }
+
+    [Fact]
+    public void TestInput()
+    {
+        var challenge = new Challenge(@"D:\Development\AdventOfCode\AdventOfCode2021\AdventOfCode2021.Tests\Day03\input.txt");
+
+        Assert.NotEmpty(challenge.DiagnosticReports);
+        Assert.Equal(1000, challenge.DiagnosticReports.Count);
+
+        var bitsGroupedByPosition = Challenge.GetBitsGroupedByPosition(challenge.DiagnosticReports);
+
+        Assert.Equal(12, bitsGroupedByPosition.Count());
+
+        Assert.Equal("000100011100", Challenge.GetMostCommonBits(bitsGroupedByPosition));
+
+        Assert.Equal(284, challenge.GetGammaRate());
+        Assert.Equal(3811, challenge.GetEpsilonRate());
+        Assert.Equal(1082324, challenge.GetPowerConsumption());
+        Assert.Equal(486, challenge.GetOxygenRating());
+        Assert.Equal(2784, challenge.GetCO2ScrubberRating());
+        Assert.Equal(1353024, challenge.GetLifeSupportRating());
+    }
+}
---
+++ AdventOfCode2021/Day03/Challenge.cs
@@ -0,0 +1,145 @@
+﻿namespace AdventOfCode2021.Day03;
+
+using System;
+
+public class Challenge
+{
+    public List<string> DiagnosticReports { get; set; }
+
+    public Challenge(string inputFile)
+    {
+        DiagnosticReports = LoadInputs(inputFile).ToList();
+    }
+
+    private static IEnumerable<string> LoadInputs(string inputFile)
+    {
+        return ReadFromFile(inputFile);
+    }
+
+    private static IEnumerable<string> ReadFromFile(string inputFile)
+    {
+        return File.ReadAllLines(inputFile);
+    }
+
+    public static IEnumerable<IEnumerable<char>> GetBitsGroupedByPosition(List<string> diagnosticReports)
+    {
+        var bitsMatrix = new List<char>[diagnosticReports.First().Length];
+
+        for (int i = 0; i < bitsMatrix.Length; i++)
+        {
+            bitsMatrix[i] = new List<char>();
+        }
+
+        foreach (var diagnosticReport in diagnosticReports)
+        {
+            var diagnosticReportArray = diagnosticReport.ToArray();
+
+            for (int i = 0; i < diagnosticReport.Length; i++)
+            {
+                bitsMatrix[i].Add(diagnosticReportArray[i]);
+            }
+        }
+
+        return bitsMatrix.ToList();
+    }
+
+    public int GetGammaRate()
+    {
+        var bitsGroupedByPosition = GetBitsGroupedByPosition(DiagnosticReports);
+        var mostCommonBits = GetMostCommonBits(bitsGroupedByPosition);
+        return Convert.ToInt32(mostCommonBits, 2);
+    }
+
+    public int GetOxygenRating()
+    {
+        var oxygenDiagnosticReports = DiagnosticReports;
+
+        for (int i = 0; i < DiagnosticReports.First().Length; i++)
+        {
+            var bitsGroupedByPosition = GetBitsGroupedByPosition(oxygenDiagnosticReports);
+            var mostCommonBits = GetMostCommonBits(bitsGroupedByPosition);
+
+            var mostCommonBit = mostCommonBits[i];
+            oxygenDiagnosticReports = oxygenDiagnosticReports.Where(x => x[i] == mostCommonBit).ToList();
+
+            if (oxygenDiagnosticReports.Count == 1)
+            {
+                break;
+            }
+        }
+
+        return Convert.ToInt32(oxygenDiagnosticReports.First(), 2);
+    }
+
+    public int GetLifeSupportRating()
+    {
+        return GetOxygenRating() * GetCO2ScrubberRating();
+    }
+
+    public int GetCO2ScrubberRating()
+    {
+        var oxygenDiagnosticReports = DiagnosticReports;
+
+        for (int i = 0; i < DiagnosticReports.First().Length; i++)
+        {
+            var bitsGroupedByPosition = GetBitsGroupedByPosition(oxygenDiagnosticReports);
+            var leastCommonBits = GetLeastCommonBits(bitsGroupedByPosition);
+
+            var leastCommonBit = leastCommonBits[i];
+            oxygenDiagnosticReports = oxygenDiagnosticReports.Where(x => x[i] == leastCommonBit).ToList();
+
+            if (oxygenDiagnosticReports.Count == 1)
+            {
+                break;
+            }
+        }
+
+        return Convert.ToInt32(oxygenDiagnosticReports.First(), 2);
+    }
+
+    public int GetEpsilonRate()
+    {
+        var bitsGroupedByPosition = GetBitsGroupedByPosition(DiagnosticReports);
+        var leastCommonBits = GetLeastCommonBits(bitsGroupedByPosition);
+        return Convert.ToInt32(leastCommonBits, 2);
+    }
+
+    private static string GetLeastCommonBits(IEnumerable<IEnumerable<char>> bitsGroupedByPosition)
+    {
+        var mostCommonBits = new List<string>();
+
+        foreach (var bits in bitsGroupedByPosition)
+        {
+            mostCommonBits.Add(GetLeastCommonBit(bits));
+        }
+
+        return string.Join("", mostCommonBits);
+    }
+
+    public static string GetMostCommonBits(IEnumerable<IEnumerable<char>> bitsGroupedByPosition)
+    {
+        var mostCommonBits = new List<string>();
+
+        foreach (var bits in bitsGroupedByPosition)
+        {
+            mostCommonBits.Add(GetMostCommonBit(bits));
+        }
+
+        return string.Join("", mostCommonBits);
+    }
+
+    public static string GetMostCommonBit(IEnumerable<char> bits)
+    {
+        return bits.Where(x => x == '0').Count() > bits.Where(x => x == '1').Count() ? "0" : "1";
+    }
+
+    public static string GetLeastCommonBit(IEnumerable<char> bits)
+    {
+        return bits.Where(x => x == '0').Count() <= bits.Where(x => x == '1').Count() ? "0" : "1";
+    }
+
+    public int GetPowerConsumption()
+    {
+        return GetEpsilonRate() * GetGammaRate();
+    }
+}
